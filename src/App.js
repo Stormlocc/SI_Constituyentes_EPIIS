6eb5695dc9b7ddda68d9f88d6e36db8783521b6b
Anthony Mayron Lopez <anthonymlocc@hotmail.com>
2023-07-12 01:47:28 -0500

    Version 1 con adminLTE

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './componentes/Header'
+import Aside from './componentes/Aside'
+import Content from './componentes/Content'
+import Footer from './componentes/Footer'
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Aside/>
+      <Content/>
+      <Footer/>
+    </div>
+  )
+}
